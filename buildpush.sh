acdcc32aa2e624ca7914ff314eb70c012239c950
emjburns <emjeburns@gmail.com>
2016-10-05 15:31:54 -0700

    update buildpush

--- buildpush.sh
+++ buildpush.sh
@@ -6,7 +6,13 @@ DOCKER_USERNAME=${DOCkER_USERNAME:-emjburns}
 DOCKER_PASS=${DOCKER_PASS:-null}
 BUILD_NUMBER=${BUILD_NUMBER:-`date "+%Y%m%d-%H%M%S"`}
 
+#clean up login
+rm -rf ~/.docker
+
 $SUDO docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASS"
 $SUDO docker build -t emjburns/spin-kub-demo:v$BUILD_NUMBER .
+
+if [ ! -f ~/.docker/config.json ] exit 1
+
 $SUDO docker push emjburns/spin-kub-demo:v$BUILD_NUMBER
 
